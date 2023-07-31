627735541b123096020eafcfcc6f92ca48c5807d
karlley <karlley.jp@gmail.com>
2023-07-31 20:35:28 +0900

    Railsを使ってタスク作成機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,16 @@ class TasksController < ApplicationController
     #取得したレコードをJSONで返す
     render json: tasks
   end
+
+  def create
+    Task.create(task_params)
+    #ヘッダのレスポンスのみを返す
+    head :created
+  end
+
+  private
+
+  def task_params
+    params.permit(:name, :is_done)
+  end
 end
--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
 import Task from "./components/Task";
-import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Center,
+  Box,
+  CheckboxGroup,
+  Text,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 import axios from "axios";
 
 const App = () => {
   //タスクの状態管理用の値、更新用の関数を定義
   const [tasks, setTasks] = useState([]);
+  //入力部の状態管理用の値、更新用の関数を定義
+  const [name, setName] = useState("");
 
   //初期値を取得
   const fetch = async () => {
@@ -13,6 +23,18 @@ const App = () => {
     const res = await axios.get("http://localhost:3010/tasks");
     //取得した値で初期値を更新
     setTasks(res.data);
+  };
+
+  //タスクを作成
+  const createTask = async () => {
+    await axios.post("http://localhost:3010/tasks", {
+      name: name,
+      is_done: false,
+    });
+    //タスク作成後に入力部を空にする
+    setName("");
+    //作成したタスクを含めて初期値を再取得
+    fetch();
   };
 
   //初回レンダリング時に初期値をセット
@@ -23,7 +45,7 @@ const App = () => {
   //タスクが完了の値isDoneを更新する関数
   const toggleIsDone = (index) => {
     const tasksCopy = [...tasks];
-    const isDone = tasksCopy[index].isDone;
+    const isDone = tasksCopy[index].is_done;
     tasksCopy[index].isDone = !isDone; //isDoneプロパティのtrue/falseを反転
     setTasks(tasksCopy); //反転した値で状態更新
   };
@@ -37,6 +59,19 @@ const App = () => {
               タスク一覧
             </Text>
           </Box>
+          <Flex mb="24px">
+            <Input
+              placeholder="タスク名を入力"
+              value={name}
+              //inputに入力された値でタスク名のstateを更新
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Box ml="16px">
+              <Button colorScheme="teal" onClick={createTask}>
+                タスクを作成
+              </Button>
+            </Box>
+          </Flex>
           <CheckboxGroup>
             {tasks.map((task, index) => {
               return (
@@ -44,7 +79,7 @@ const App = () => {
                   key={index}
                   index={index}
                   name={task.name}
-                  isDone={task.isDone}
+                  isDone={task.is_done}
                   toggleIsDone={toggleIsDone}
                 />
               );
